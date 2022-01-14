3301cc31f0e7096b12aca68f0ead3aeb1c6d9072
André de Sousa Lacerda <a.de.sousa.lacerda@avanade.com>
2022-01-14 17:08:40 -0300

    Melhorias no projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './views/shared/top-bar/top-bar.component';
 import { FeedComponent } from './views/pages/feed/feed.component';
 import { PostComponent } from './views/shared/post/post.component';
+import { CommentComponent } from './views/shared/comment/comment.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +13,7 @@ import { PostComponent } from './views/shared/post/post.component';
     TopBarComponent,
     FeedComponent,
     PostComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
--- src/app/views/shared/post/post.component.ts
+++ src/app/views/shared/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
+@Input() post: any
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  sendComment = () => {
+    alert("Enviando um comentário")
   }
 
 }
